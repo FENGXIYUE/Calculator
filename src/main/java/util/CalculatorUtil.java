c4cbc612d27099ec7bf79c045f9cb0f1ed5a50b6
LIULONGTAO <nwpullt@163.com>
2021-07-16 16:09:42 +0800

    [20210716]-刘龙涛-注释修改

--- src/main/java/util/CalculatorUtil.java
+++ src/main/java/util/CalculatorUtil.java
@@ -32,13 +32,13 @@ public class CalculatorUtil {
         for (int i = 0; i < expression.length(); i++) {
             char ch = expression.charAt(i); // 获取一个字符
             char lastCh = expression.charAt(i > 1 ? i - 1 : 0);
-            if (i == 0 && ('-' == ch || '+' == ch)) {
+            if (i == 0 && ('-' == ch || '+' == ch)) { //包含正负数
                 temp.append(ch);
             } else if ('(' == lastCh && ('-' == ch || '+' == ch)) {
                 temp.append(ch);
             } else if (StringUtils.isNumeric(String.valueOf(ch))) { // 若当前字符是数字
                 temp.append(ch); // 加入到数字缓存中
-            } else if ('.' == ch) {
+            } else if ('.' == ch) {//加小数点
                 temp.append(ch);
             } else { // 非数字的情况
                 String tempStr = temp.toString(); // 将数字缓存转为字符串
